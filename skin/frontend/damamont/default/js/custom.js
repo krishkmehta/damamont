6312b5a54a9fda9b5bbec2f8d34bf4f97c624744
krishkmehta <krishankm92@gmail.com>
2019-04-06 10:18:00 -0400

    contact page and home page

---
+++ skin/frontend/damamont/default/js/custom.js
@@ -0,0 +1,29 @@
+jQuery('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+jQuery(window).scroll(function() {    
+    var scroll = jQuery(window).scrollTop();
+
+     //>=, not <=
+     if (scroll >= 100) {
+        //clearHeader, not clearheader - caps H
+        jQuery(".header-container").addClass("is_active");
+    }
+    else{
+       jQuery(".header-container").removeClass("is_active");   
+   }
+}); //missing );
