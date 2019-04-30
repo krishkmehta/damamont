becf52ac4a4c734c724f95e979fa8703bf805e31
krishkmehta <krishankm92@gmail.com>
2019-04-29 21:16:59 -0400

    Flag Design WIP

--- skin/frontend/rwd/default/js/slideshow.js
+++ skin/frontend/rwd/default/js/slideshow.js
@@ -29,16 +29,16 @@ $j(document).ready(function () {
     // UI Pattern - Slideshow
     // ==============================================
 
-    $j('.slideshow-container .slideshow')
-        .cycle({
-            slides: '> li',
-            pager: '.slideshow-pager',
-            pagerTemplate: '<span class="pager-box"></span>',
-            speed: 600,
-            pauseOnHover: true,
-            swipe: true,
-            prev: '.slideshow-prev',
-            next: '.slideshow-next',
-            fx: 'scrollHorz'
-        });
+    // $j('.slideshow-container .slideshow')
+    //     .cycle({
+    //         slides: '> li',
+    //         pager: '.slideshow-pager',
+    //         pagerTemplate: '<span class="pager-box"></span>',
+    //         speed: 600,
+    //         pauseOnHover: true,
+    //         swipe: true,
+    //         prev: '.slideshow-prev',
+    //         next: '.slideshow-next',
+    //         fx: 'scrollHorz'
+    //     });
 });
